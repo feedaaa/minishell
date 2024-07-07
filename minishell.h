bac209eaf9b546c7efc050168889ec137006399a
Aysha0001 <ayshaemadb@gmail.com>
2024-07-07 16:16:24 +0400

    Update minishell.h

--- minishell.h
+++ minishell.h
@@ -21,4 +21,19 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+typedef enum e_operator {
+	NONE,                   //No specific operator.
+	RDR_OUT_REPLACE,        //> in shells
+	RDR_OUT_APPEND,        //>> in shells
+	RDR_INPUT,             //< in shells
+	RDR_INPUT_UNTIL,       //| in shells
+	PIPE,
+}				t_operator;
+
+typedef struct s_commandinput {
+    char *command;          // Command string
+    t_operator operator;    // Operator associated with the command
+    struct s_commandinput *next; // Pointer to the next command(for example: cat file.txt | grep "anything") It will point to the command after the operator
+} t_commandinput;
+
 #endif
