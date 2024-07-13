836bf41f3eeb1bc8242c0a5d68cca995f4e90d43
Aysha0001 <ayshaemadb@gmail.com>
2024-07-13 16:47:09 +0400

    Create Parser_utils.c

---
+++ Parser_utils.c
@@ -0,0 +1,99 @@
+//represents the number of command-line arguments passed to the program.
+size_t	get_argc(char **parsed)
+{
+	size_t	i;
+
+	i = 0;
+	while (parsed[i])
+		i += 1;
+	return (i);
+}
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	while (*s1 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return ((unsigned char)*s1 - (unsigned char)*s2);
+}
+
+t_operator	get_operator(char *operator)
+{
+	t_operator	op;
+
+	if (!operator)
+		op = NONE;
+	else if (!ft_strcmp(operator, "|"))
+		op = PIPE;
+	else if (!ft_strcmp(operator, ">>"))
+		op = RDR_OUT_APPEND;
+	else if (!ft_strcmp(operator, ">"))
+		op = RDR_OUT_REPLACE;
+	else if (!ft_strcmp(operator, "<<"))
+		op = RDR_INPUT_UNTIL;
+	else if (!ft_strcmp(operator, "<"))
+		op = RDR_INPUT;
+	else
+		op = NONE;
+	free(operator);
+	return (op);
+}
+
+
+size_t	remove_quotes_size(char	*parsed)
+{
+	size_t	i;
+	size_t	size;
+	char	quotes;
+
+	i = 0;
+	size = 0;
+	while (parsed[i])
+	{
+		while (parsed[i] && !is_therechar(QUOTES, parsed[i]))
+		{
+			i += 1;
+			size += 1;
+		}
+		if (!parsed[i])
+			break ;
+		quotes = parsed[i++];
+		while (parsed[i] && parsed[i] != quotes)
+		{
+			i += 1;
+			size += 1;
+		}
+		quotes = 0;
+	}
+	return (size);
+}
+
+char	*remove_quotes(char	*parsed)
+{
+	size_t	i;
+	size_t	j;
+	char	quotes;
+	char	*unquoted_parsed;
+
+	i = 0;
+	j = 0;
+	quotes = '\0';
+	unquoted_parsed = malloc((remove_quotes_size(parsed) + 1) * sizeof(char));
+	while (parsed[i])
+	{
+		while (parsed[i] && !is_therechar(QUOTES, parsed[i]))
+			unquoted_parsed[j++] = parsed[i++];
+		if (!parsed[i])
+			break ;
+		quotes = parsed[i];
+		i += 1;
+		while (parsed[i] && parsed[i] != quotes)
+			unquoted_parsed[j++] = parsed[i++];
+		quotes = '\0';
+	}
+	unquoted_parsed[j] = '\0';
+	free(parsed);
+	return (unquoted_parsed);
+}
