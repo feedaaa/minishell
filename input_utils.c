5367648f4c63c600a313704d9be5c0b019edfa67
Aysha0001 <ayshaemadb@gmail.com>
2024-08-14 17:04:39 +0400

    Create input_utils.c

---
+++ input_utils.c
@@ -0,0 +1,82 @@
+#include "minishell.h"
+
+
+int	unclosed_quotes(char *str)
+{
+	char	last_opened;
+
+	last_opened = 0;
+	while (*str && !last_opened)
+	{
+		if (*str == '\'' || *str == '"')
+			last_opened = *str;
+		str++;
+	}
+	while (*str && last_opened)
+	{
+		if (*str && *str == last_opened)
+			last_opened = 0;
+		str++;
+	}
+	if (*str)
+		return (unclosed_quotes(str));
+	else if (!last_opened)
+		return (0);
+	else
+		return (1);
+}
+
+void	free_matrix(char **matrix)
+{
+	size_t	i;
+
+	i = 0;
+	if (!matrix)
+		return ;
+	while (matrix[i])
+	{
+		free(matrix[i]);
+		matrix[i] = NULL;
+		i += 1;
+	}
+	free(matrix);
+	matrix = NULL;
+}
+
+void	p_lstclear(t_statement **head)
+{
+	t_statement	*temp;
+	t_statement	*next_node;
+
+	if (!head)
+		return ;
+	temp = *head;
+	while (temp != NULL)
+	{
+		next_node = temp->next;
+		free_matrix(temp->argv);
+		free(temp);
+		temp = next_node;
+	}
+	*head = NULL;
+}
+
+
+void	v_lstclear(t_vlst **head)
+{
+	t_vlst	*temp;
+	t_vlst	*next_node;
+
+	if (!head)
+		return ;
+	temp = *head;
+	while (temp != NULL)
+	{
+		next_node = temp->next;
+		free(temp->var_name);
+		free(temp->var_value);
+		free(temp);
+		temp = next_node;
+	}
+	*head = NULL;
+}
