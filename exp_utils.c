f7022fd64bf7cfebc4e541c7b47aac956cba34c6
Aysha0001 <ayshaemadb@gmail.com>
2024-08-14 17:02:00 +0400

    Update exp_utils.c

--- exp_utils.c
+++ exp_utils.c
@@ -48,3 +48,62 @@ bool	single_dollar(char *input_at_i)
 			|| input_at_i[1] == ' '
 			|| input_at_i[1] == '\"'));
 }
+
+bool	streq(char *str1, char *str2)
+{
+	size_t	i;
+
+	if ((str1 && !str2) || (!str1 && str2))
+		return (false);
+	i = 0;
+	while (str1[i] || str2[i])
+	{
+		if (str1[i] != str2[i])
+			return (false);
+		i += 1;
+	}
+	return (true);
+}
+
+static long long	ft_digits(long long n)
+{
+	long long	digits;
+
+	digits = 0;
+	if (n <= 0)
+		digits += 1;
+	while (n != 0)
+	{
+		n /= 10;
+		digits += 1;
+	}
+	return (digits);
+}
+
+char	*ft_lltoa(long long n)
+{
+	long long	digits;
+	int			signal;
+	char		*result;
+
+	digits = ft_digits(n);
+	signal = 1;
+	result = malloc((digits + 1) * sizeof(char));
+	if (!result)
+		return (NULL);
+	result[digits--] = '\0';
+	if (n < 0)
+	{
+		signal = -1;
+		result[0] = '-';
+	}
+	else if (n == 0)
+		result[0] = '0';
+	while (n != 0)
+	{
+		result[digits--] = (n % 10 * signal) + '0';
+		n /= 10;
+	}
+	return (result);
+}
+
