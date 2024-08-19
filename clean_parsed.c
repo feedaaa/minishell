c21cbfb0d1551247758d209ac52ab20bfb739680
Aysha0001 <ayshaemadb@gmail.com>
2024-08-20 01:26:35 +0400

    Update clean_parsed.c

--- clean_parsed.c
+++ clean_parsed.c
@@ -6,38 +6,3 @@ void	clean_parsed(t_statement **statement_list, t_data *data)
 	*statement_list = NULL;
 	data->head = NULL;
 }
-
-void	p_lstclear(t_statement **head)
-{
-	t_statement	*temp;
-	t_statement	*next_node;
-
-	if (!head)
-		return ;
-	temp = *head;
-	while (temp != NULL)
-	{
-		next_node = temp->next;
-		free_matrix(temp->argv);
-		free(temp);
-		temp = next_node;
-	}
-	*head = NULL;
-}
-
-void	free_matrix(char **matrix)
-{
-	size_t	i;
-
-	i = 0;
-	if (!matrix)
-		return ;
-	while (matrix[i])
-	{
-		free(matrix[i]);
-		matrix[i] = NULL;
-		i += 1;
-	}
-	free(matrix);
-	matrix = NULL;
-}
