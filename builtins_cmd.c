d050eb7ae98dd8eca30f7379d1eecbc0e3a427e7
Aysha0001 <ayshaemadb@gmail.com>
2024-08-25 03:01:35 +0400

    Create builtins_cmd.c

---
+++ builtins_cmd.c
@@ -0,0 +1,46 @@
+#include "minishell.h"
+
+static int	cd_too_many_args(void)
+{
+	ft_putendl_fd(CD_TOO_MANY_ARGS, STDERR_FILENO);
+	return (EXIT_FAILURE);
+}
+
+
+int	call_cmd_echo(t_statement *s)
+{
+	t_statement	*temp;
+	bool		has_n;
+
+	temp = s;
+	has_n = false;
+	if (s->argc >= 2)
+		has_n = streq(s->argv[1], "-n");
+	cmd_echo(temp, has_n);
+	temp = temp->next;
+	while (temp != NULL && temp->argc > 2)
+	{
+		cmd_echo(temp, false);
+		if (temp->operator == PIPE)
+			break ;
+		temp = temp->next;
+	}
+	if (!has_n)
+		ft_putchar_fd('\n', STDOUT_FILENO);
+	return (EXIT_SUCCESS);
+}
+
+int	call_cmd_cd(t_statement *s, t_data *data)
+{
+	if (s->argc > 2)
+		return (cd_too_many_args());
+	else
+		return (cmd_cd(s->argv[1], data));
+}
+
+int	call_cmd_unset(t_statement *s, t_data *data)
+{
+	if (s->argc == 1)
+		return (EXIT_SUCCESS);
+	return (cmd_unset(s, &data->envp_lst));
+}
