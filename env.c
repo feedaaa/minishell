268e085ef75f1b78f0b6d6571f09ff5024c9776f
Aysha0001 <ayshaemadb@gmail.com>
2024-08-25 02:57:55 +0400

    Create env.c (builtin function)

---
+++ env.c
@@ -0,0 +1,15 @@
+#include "minishell.h"
+
+int	cmd_env(t_data *data)
+{
+	t_vlst	*temp;
+
+	temp = data->envp_lst;
+	while (temp != NULL)
+	{
+		if (temp->is_exported)
+			printf("%s=%s\n", temp->var_name, temp->var_value);
+		temp = temp->next;
+	}
+	return (EXIT_SUCCESS);
+}
