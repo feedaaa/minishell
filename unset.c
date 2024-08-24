ba183898ba800454455147a0d0ee4101e61509bf
Aysha0001 <ayshaemadb@gmail.com>
2024-08-25 02:36:29 +0400

    Create unset.c (builtin function)

---
+++ unset.c
@@ -0,0 +1,14 @@
+#include "minishell.h"
+
+int	cmd_unset(t_statement *s, t_vlst **head)
+{
+	int	i;
+
+	i = 1;
+	while (i < s->argc)
+	{
+		unset_var(s->argv[i], head);
+		i += 1;
+	}
+	return (EXIT_SUCCESS);
+}
