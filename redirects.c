250e59f14d220c4a0d6931ccad806e4f8e6b9731
Aysha0001 <ayshaemadb@gmail.com>
2024-08-25 02:40:25 +0400

    Update redirects.c

--- redirects.c
+++ redirects.c
@@ -15,75 +15,97 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+#include "minishell.h"
 
-t_vlst	*v_new_node(char *var_name, char *var_value, bool is_exported)
+//reads whats inside fd[0] and write to fd[1]
+static void	redirect_input_until(t_statement *node)
 {
-	t_vlst	*new_node;
+	char	*buff;
+	int		fd[2];
 
-	new_node = malloc(sizeof(t_vlst));
-	new_node->var_name = var_name;
-	new_node->var_value = var_value;
-	new_node->is_exported = is_exported;
-	new_node->next = NULL;
-	return (new_node);
-}
-
-// This function splits an environment variable string into its name and value components,
-//storing them in a dynamically allocated array of strings.
-
-char	**split_envp(char *env)
-{
-	char	**splitted;
-	size_t	indexof_equal;
-
-
-	splitted = malloc(3 * sizeof(char *));
-		// 3 because we have the 1-name variable and 2-the expanded value and 3-the null
-	if (!splitted)
-		return (NULL);
-	indexof_equal = ft_strchr(env, '=') - env;
-	splitted[0] = ft_substr(env, 0, indexof_equal); //"NAME"starting from 0 to where there was equal sign
-	splitted[1] = ft_substr(env, indexof_equal + 1, ft_strlen(&env[indexof_equal])); //"VALUE" starting from the equal sign and afterwards
-	splitted[2] = NULL; // To indicate the end of the first environment variable
-	return (splitted);
-}
-
-// initializes a linked list of environment variables from an array of environment strings
-t_vlst	*init_envp_lst(char **envp)
-{
-	t_vlst	*head;
-	t_vlst	*temp;
-	char	**line;
-	size_t	i;
-
-	line = split_envp(envp[0]);
-		// splits the first environment variable into array of name ane the other array is the value if we assume NAME=VALUE
-	head = v_new_node(line[0], line[1], true);
-	free(line);
-	i = 1;
-	temp = head;
-	while (envp[i])
+	pipe(fd);  //for one file to use the others file input
+	while (1)
 	{
-		line = split_envp(envp[i]);
-		temp->next = v_new_node(line[0], line[1], true);
-		free(line);
-		temp = temp->next;
-		i += 1;
+		buff = readline("> ");  //store each line of input
+		if (streq(buff, node->next->argv[0])) //any of the buff is similar to the next node it breaks
+			break ;
+		ft_putendl_fd(buff, fd[1]); //writes the input stored in buff inside fd1
 	}
-	init_oldpwd(&head);
-	return (head);
+	close(fd[1]);
+	dup2(fd[0], STDIN_FILENO); //what is store in fd[0] is the input
+	close(fd[0]);
+	free(buff);
 }
 
-// initialising the shell based on the environment
-void	setup_shell(char **envp, t_data *data, t_statement **statement_list)
+//read from file insead from the standard input "<"
+static void	redirect_input(t_statement *node)
 {
-	data->envp = envp;
-	data->envp_lst = init_envp_lst(envp);
-	data->head = NULL;
-	*statement_list = NULL;
-	config_signals(); //this function to update the shell depending on some signals such as ctrl+c ...
+	int		in_file;
+	char	*error_msg_prefix;
+
+	if (node->next->argv[0])  //the first argument of the next node
+	{
+		while (node->next->operator == RDR_INPUT)  //in case there is more than one redirector 
+			node = node->next;                  //it reaches to the last redirector command
+		if (access(node->next->argv[0], F_OK) == 0) //checks if the file exists
+		{
+			in_file = open(node->next->argv[0], O_RDONLY, 0666);
+			dup2(in_file, STDIN_FILENO); //makes the file descriptor the standard input
+		}
+		else   //if the file does not exist
+		{
+			error_msg_prefix = ft_strjoin("minishell: ", node->next->argv[0]);
+			perror(error_msg_prefix);
+			free(error_msg_prefix);
+			g_exit_status = 2;
+			exit(EXIT_FAILURE);
+		}
+	}
 }
 
+// O_WRONLY: Open the file for writing only.
+// O_TRUNC: remove any content if exists.
+// O_CREAT: Create the file if it doesn’t exist.
+// 0666: Set the file’s permissions (readable and writable by everyone).
+// O_APPEND: keeps any content that exist.
+static void	redirect_output(t_statement *node)
+{
+	close(STDOUT_FILENO); //close the standard output file so than the next opened file will take its job
+	while (node->next->operator == RDR_OUT_REPLACE
+		|| node->next->operator == RDR_OUT_APPEND)
+	{
+		if (node->operator == RDR_OUT_REPLACE) //">"
+			open(node->next->argv[0], O_WRONLY | O_TRUNC | O_CREAT, 0666); //so the standart output will appear in this file replacing its content
+		else if (node->operator == RDR_OUT_APPEND) //">>"
+			open(node->next->argv[0], O_WRONLY | O_APPEND | O_CREAT, 0666);
+		node = node->next;
+		close(1);
+	}
+	if (node->operator == RDR_OUT_REPLACE)
+		open(node->next->argv[0], O_WRONLY | O_TRUNC | O_CREAT, 0666);
+	else if (node->operator == RDR_OUT_APPEND)
+		open(node->next->argv[0], O_WRONLY | O_APPEND | O_CREAT, 0666);
+}
+
+void	exec_redirects(t_statement *node, t_data *data)
+{
+	t_statement	*temp;
+
+	temp = node;
+	if (node->operator == RDR_INPUT)
+		redirect_input(node);
+	else if (node->operator == RDR_INPUT_UNTIL)
+		redirect_input_until(node);
+	else
+		redirect_output(node);
+	temp->operator = NONE; //to not mistakenly reprocess the redirection
+	while (node->operator != NONE && node->operator != PIPE)
+		node = node->next;
+	if (node->operator == NONE)
+		exec_cmd(temp, data);
+	else
+		exec_pipe(node, data);
+}
 
 
 
