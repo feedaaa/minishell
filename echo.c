105e28152e80303cb14c6ac6cd43bbb7278a3b08
Aysha0001 <ayshaemadb@gmail.com>
2024-08-25 02:58:48 +0400

    Create echo.c (builtin function)

---
+++ echo.c
@@ -0,0 +1,36 @@
+#include "minishell.h"
+
+static void	print(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\\')
+		{
+			i += 1;
+			continue ;
+		}
+		ft_putchar_fd(str[i], STDOUT_FILENO);
+		i += 1;
+	}
+}
+
+int	cmd_echo(t_statement *statement, bool has_n)
+{
+	int		i;
+
+	i = 1;
+	if (has_n)
+		i += 1;
+	while (statement->argv[i] && i != statement->argc)
+	{
+		print(statement->argv[i]);
+		if (!statement->argv[i + 1] && statement->operator != RDR_INPUT)
+			break ;
+		ft_putchar_fd(' ', STDOUT_FILENO);
+		i += 1;
+	}
+	return (EXIT_SUCCESS);
+}
