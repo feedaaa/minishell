8ced40bcf60e98d700395d161972795d410436ff
Aysha0001 <ayshaemadb@gmail.com>
2024-08-29 02:05:52 +0400

    Handle when there is a redirector after a "|"

--- parser/invalid_syntax.c
+++ parser/invalid_syntax.c
@@ -14,23 +14,76 @@ bool	has_operator(char *input)
 	return (false);
 }
 
-bool	check_operator_syntax(char *input, size_t i)
+// bool	check_operator_syntax(char *input, size_t i)
+// {
+// 	if (input[i] == input[i + 1])
+// 		i += 2;
+// 	else
+// 		i++;
+// 	if (input[i] == ' ')
+// 	{
+// 		while (input[i] && input[i] == ' ')
+// 			(i)++;
+// 		if (is_therechar(OPERATORS, input[i]))
+// 			return (unexpected_token(input[i]));
+// 	}
+// 	if (is_therechar(OPERATORS, input[i]))
+// 		return (unexpected_token(input[i]));
+// 	return (false);
+// }
+
+static bool	handle_pipe_and_spaces(char *input, size_t *i)
 {
-	if (input[i] == input[i + 1])
-		i += 2;
-	else
-		i++;
-	if (input[i] == ' ')
+	(*i)++;
+	while (input[*i] && input[*i] == ' ')
+		(*i)++;
+	if (is_therechar(REDIRECTS, input[*i]))
+		return (false);
+	return (true);
+}
+
+static bool	check_for_operator_syntax(char *input, size_t *i)
+{
+	if (input[*i] == ' ')
 	{
-		while (input[i] && input[i] == ' ')
-			(i)++;
-		if (is_therechar(OPERATORS, input[i]))
-			return (unexpected_token(input[i]));
+		while (input[*i] && input[*i] == ' ')
+			(*i)++;
 	}
-	if (is_therechar(OPERATORS, input[i]))
-		return (unexpected_token(input[i]));
+	if (is_therechar(OPERATORS, input[*i]))
+		return (unexpected_token(input[*i]));
 	return (false);
 }
+
+bool	invalid_syntax_on_operator(char *input)
+{
+	size_t	i;
+	bool	in_quotes;
+
+	i = 0;
+	in_quotes = false;
+	while (has_operator(&input[i]))
+	{
+		if (is_therechar(QUOTES, input[i]))
+			in_quotes = !in_quotes;
+		if (input[i] == input[i + 1])
+			i += 2;
+		if (is_therechar(OPERATORS, input[i]) && !in_quotes)
+		{
+			if (input[i] == '|')
+			{
+				if (!handle_pipe_and_spaces(input, &i))
+					return (false);
+			}
+			else
+				i += 1;
+			if (check_for_operator_syntax(input, &i))
+				return (true);
+		}
+		i += 1;
+	}
+	return (false);
+}
+
 
 bool	invalid_syntax_on_operator(char *input)
 {
